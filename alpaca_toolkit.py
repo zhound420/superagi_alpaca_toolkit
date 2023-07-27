56a5ad4abb0ab86aa35928f57c0121333d1d51b3
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-07-26 20:40:59 -0700

    Update alpaca_toolkit.py

--- alpaca_toolkit.py
+++ alpaca_toolkit.py
@@ -1,15 +1,15 @@
 
 import os
 from superagi.tools.base_tool import BaseToolkit
-from .alpaca_get_account_information_tool import AlpacaGetAccountInformationTool
-from .alpaca_close_trade_tool import AlpacaCloseTradeTool
-from .alpaca_place_trade_tool import AlpacaPlaceTradeTool
-from .alpaca_close_all_trades_tool import AlpacaCloseAllTradesTool
-from .alpaca_get_positions_tool import AlpacaGetPositionsTool
-from .alpaca_monitor_tool import AlpacaMonitorTool
-from .alpaca_check_price_changes_tool import AlpacaCheckPriceChangesTool
-from .alpaca_get_current_price_tool import AlpacaGetCurrentPriceTool
-from .alpaca_get_day_percent_change_tool import AlpacaGetDayPercentChangeTool
+from alpaca_get_account_information_tool import AlpacaGetAccountInformationTool
+from alpaca_close_trade_tool import AlpacaCloseTradeTool
+from alpaca_place_trade_tool import AlpacaPlaceTradeTool
+from alpaca_close_all_trades_tool import AlpacaCloseAllTradesTool
+from alpaca_get_positions_tool import AlpacaGetPositionsTool
+from alpaca_monitor_tool import AlpacaMonitorTool
+from alpaca_check_price_changes_tool import AlpacaCheckPriceChangesTool
+from alpaca_get_current_price_tool import AlpacaGetCurrentPriceTool
+from alpaca_get_day_percent_change_tool import AlpacaGetDayPercentChangeTool
 
 # Set environment variables
 os.environ['APCA_API_KEY_ID'] = 'Your Alpaca API Key ID'
