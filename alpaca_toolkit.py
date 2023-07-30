361947a592a0fe79c47c4799a4be0c90456740e3
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-07-30 00:24:36 -0700

    Update alpaca_toolkit.py

--- alpaca_toolkit.py
+++ alpaca_toolkit.py
@@ -1,34 +1,43 @@
+"""
+This file defines the Alpaca toolkit.
+"""
+
 from abc import ABC
 from typing import Any, Dict, List
 from pydantic import BaseModel
 from superagi.tools.base_tool import BaseToolkit, BaseTool
 
-class AlpacaTool(BaseModel):
+class AlpacaTool(BaseToolkit, ABC):
     """
-    This is the AlpacaTool class.
+    This class represents the Alpaca toolkit.
     """
     name: str
     description: str
     tools: List[BaseTool]
 
-class Config:(BaseModel):
-    """
-    This is the Config: class.
-    """
+    class Config:
+        """
+        This is a Config class for AlpacaTool.
+        """
         arbitrary_types_allowed = True
 
-    def get_tools(self):
+    def get_tools(self) -> Dict[str, Any]:
         """
-        This is the get_tools method of the Config: class.
+        Returns a dictionary of tools in the toolkit.
         """
         return {tool.name: tool for tool in self.tools}
 
-    def get_tool(self):
+    def get_tool(self, name: str) -> BaseTool:
         """
-        This is the get_tool method of the Config: class.
+        Returns a tool from the toolkit by name.
         """
         return self.get_tools()[name]
 
+    def get_env_keys(self) -> List[str]:
+        """
+        Returns a list of environment variable keys required by the toolkit.
+        """
+        return ['APCA_API_KEY_ID', 'APCA_API_SECRET_KEY', 'APCA_API_BASE_URL', 'APCA_API_DATA_URL', 'APCA_PAPER']
+
 tools = [AlpacaTool(name='AlpacaTool', description='Description')]
 toolkit = BaseToolkit(name='AlpacaToolkit', description='Description', tools=tools)
-
