fe2d3efd19b017b19cd25891a423d0a8e7636629
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-07-30 20:27:43 -0700

    v3

--- alpaca_toolkit.py
+++ alpaca_toolkit.py
@@ -3,32 +3,21 @@ from typing import Any, Dict, List
 from pydantic import BaseModel
 from superagi.tools.base_tool import BaseToolkit, BaseTool
 
-class AlpacaTool(BaseModel):
+class AlpacaToolkit(BaseToolkit, ABC):
     """
-    This is the AlpacaTool class.
+    This is the AlpacaToolkit class.
     """
-    name: str
-    description: str
-    tools: List[BaseTool] = [MyTool()]
+    name: str = "AlpacaToolkit"
+    description: str = "Toolkit for Alpaca trading tools."
 
-class Config(BaseModel):
-    """
-    This is the Config: class.
-    """
-    arbitrary_types_allowed = True
+    def get_tools(self) -> List[BaseTool]:
+        # List of tools should be updated to include all the tool instances
+        return []
 
-    def get_tools(self):
-        """
-        This is the get_tools method of the Config: class.
-        """
-        return {tool.name: tool for tool in self.tools}
+    def get_env_keys(self) -> List[str]:
+        # List of environment variable keys should be updated as per requirements
+        return []
 
-    def get_tool(self):
-        """
-        This is the get_tool method of the Config: class.
-        """
-        return self.get_tools()[name]
 
-tools = [AlpacaTool(name='AlpacaTool', description='Description')]
-toolkit = BaseToolkit(name='AlpacaToolkit', description='Description', tools=tools)
-
+tools = []  # List of tools should be updated to include all the tool instances
+toolkit = AlpacaToolkit()
