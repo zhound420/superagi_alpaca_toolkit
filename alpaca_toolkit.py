f5150130597aa5a162fea896746c614a4f3f37d9
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-07-31 20:24:18 -0700

    Update alpaca_toolkit.py

--- alpaca_toolkit.py
+++ alpaca_toolkit.py
@@ -1,14 +1,14 @@
 from superagi.tools.base_tool import BaseToolkit, BaseTool
 from typing import Type, List
-from .alpaca_get_account_information_tool import AlpacaGetAccountInformationTool
-from .alpaca_get_positions_tool import AlpacaGetPositionsTool
-from .alpaca_monitor_tool import AlpacaMonitorTool
-from .alpaca_place_trade_tool import AlpacaPlaceTradeTool
-from .alpaca_close_trade_tool import AlpacaCloseTradeTool
-from .alpaca_close_all_trades_tool import AlpacaCloseAllTradesTool
-from .alpaca_get_day_percent_change_tool import AlpacaGetDayPercentChangeTool
-from .alpaca_get_current_price_tool import AlpacaGetCurrentPriceTool
-from .alpaca_check_price_changes_tool import AlpacaCheckPriceChangesTool
+from alpaca_get_account_information_tool import AlpacaGetAccountInformationTool
+from alpaca_get_positions_tool import AlpacaGetPositionsTool
+from alpaca_monitor_tool import AlpacaMonitorTool
+from alpaca_place_trade_tool import AlpacaPlaceTradeTool
+from alpaca_close_trade_tool import AlpacaCloseTradeTool
+from alpaca_close_all_trades_tool import AlpacaCloseAllTradesTool
+from alpaca_get_day_percent_change_tool import AlpacaGetDayPercentChangeTool
+from alpaca_get_current_price_tool import AlpacaGetCurrentPriceTool
+from alpaca_check_price_changes_tool import AlpacaCheckPriceChangesTool
 
 class AlpacaToolkit(BaseToolkit):
     name: str = "Alpaca Toolkit"
