55550c2bfa506a6ab835eeb143803bea1550b8c7
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-08-13 15:53:52 -0700

    Update alpaca_toolkit.py

--- alpaca_toolkit.py
+++ alpaca_toolkit.py
@@ -16,10 +16,10 @@ def __init__(self, session, organisation):
 def register(self):
         try:
             toolkit = create_toolkit(self.session, self.organisation, Toolkit(
-                name=self.__toolkit_name__, description=self.__toolkit_description__, version=self.__toolkit_version__))
+                name=self.name, description=self.description, version=self.toolkit_version))
             self.register_tools(toolkit)
         except Exception as e:
-            raise HTTPException(status_code=400, detail=f"Error registering {self.__toolkit_name__} toolkit: {e}")
+            raise HTTPException(status_code=400, detail=f"Error registering {self.name} toolkit: {e}")
 
 def register_tools(self, toolkit: Toolkit):
         tools = get_tools_for_toolkit(self.session, toolkit.id)
@@ -36,4 +36,4 @@ def get_tool(self, tool_name: str):
         if tool_name == "Alpaca Monitor Tool":
             return AlpacaMonitorTool()
         else:
-            raise HTTPException(status_code=404, detail=f"Tool {tool_name} not found in {self.__toolkit_name__} toolkit")
+            raise HTTPException(status_code=404, detail=f"Tool {tool_name} not found in {self.name} toolkit")
