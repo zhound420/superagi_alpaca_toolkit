a247631bc071112c628573a0201e8c18bb7e3f97
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-08-13 02:02:30 -0700

    Update alpaca_monitor_tool.py

--- alpaca_monitor_tool.py
+++ alpaca_monitor_tool.py
@@ -1,44 +1,24 @@
 
-from typing import Any, Dict, List
+from datetime import datetime
+from superagi.helper.tool_helper import BaseTool
+from superagi.tools.external_tools.superagi_alpaca_toolkit.alpaca_toolkit import AlpacaToolkit
+from alpaca import AlpacaRest, AlpacaStream
 
-from alpaca import rest
-from alpaca.entity import Account, Order
+class AlpacaMonitorTool(BaseTool):
+    def __init__(self, toolkit: AlpacaToolkit):
+        self.alpaca = toolkit.alpaca
 
-from superagi.tools.toolkit import BaseToolkit
+    async def stream(self):
+        async def on_trade_update(data):
+            print(data)
 
+        stream = AlpacaStream(
+            key_id=self.alpaca.key_id,
+            secret_key=self.alpaca.secret_key,
+            base_url=self.alpaca.base_url,
+            use_polygon=self.alpaca.use_polygon,
+        )
 
-class AlpacaMonitorTool(BaseToolkit):
-    TOOL_NAME = "Alpaca Monitor Tool"
-    TOOL_ID = "alpaca_monitor_tool"
-    TOOL_DESCRIPTION = "A tool to monitor Alpaca trades in real-time."
-    TOOL_INPUTS = []
-    TOOL_OUTPUTS = ['trade_updates']
-
-    def __init__(self, api_key: str, secret_key: str, base_url: str = 'https://paper-api.alpaca.markets'):
-        self.api_key = api_key
-        self.secret_key = secret_key
-        self.base_url = base_url
-        self.client = rest.REST(key_id=self.api_key, secret_key=self.secret_key, base_url=self.base_url)
-
-    def get_account(self) -> Account:
-        return self.client.get_account()
-
-    def list_orders(self, status: str = 'open', limit: int = 50) -> List[Order]:
-        return self.client.list_orders(status=status, limit=limit)
-
-    def execute(self, input_data: Dict[str, Any]) -> Dict[str, Any]:
-        # For now, just returning the account details as an example.
-        # The WebSocket streaming will be implemented separately.
-        account = self.get_account()
-        return {
-            'trade_updates': {
-                'account': {
-                    'id': account.id,
-                    'status': account.status,
-                    'cash': account.cash,
-                    'portfolio_value': account.portfolio_value,
-                    'buying_power': account.buying_power
-                }
-            }
-        }
+        await stream.subscribe_trade_updates(on_trade_update)
+        await stream.consume()
 
