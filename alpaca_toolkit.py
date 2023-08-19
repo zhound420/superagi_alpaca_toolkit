49f3c2e6f0c0f5fa37b463c096abb1231fb9c252
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-08-18 23:01:50 -0700

    Update alpaca_toolkit.py

--- alpaca_toolkit.py
+++ alpaca_toolkit.py
@@ -1,7 +1,12 @@
-from superagi.tools.base_tool import BaseToolkit, BaseTool
-class AlpacaBaseToolkit(BaseToolkit, ABC):
-        name: str = "Alpaca BaseToolkit"
-        description: str = "BaseToolkit for Alpaca trading platform"
+from abc import ABC
+from typing import List
+from fastapi import HTTPException
+from superagi.tools.base_tool import BaseToolkit
+from alpaca_monitor_tool import AlpacaMonitorTool
+
+class AlpacaToolkit(BaseToolkit, ABC):
+        name: str = "Alpaca Toolkit"
+        description: str = "Toolkit for Alpaca trading platform"
         toolkit_version: str = "1.0.0"
 
 def __init__(self, session, organisation):
@@ -10,13 +15,13 @@ def __init__(self, session, organisation):
 
 def register(self):
         try:
-            toolkit = create_toolkit(self.session, self.organisation, BaseToolkit(
+            toolkit = create_toolkit(self.session, self.organisation, Toolkit(
                 name=self.name, description=self.description, version=self.toolkit_version))
             self.register_tools(toolkit)
         except Exception as e:
             raise HTTPException(status_code=400, detail=f"Error registering {self.name} toolkit: {e}")
 
-def register_tools(self, toolkit: BaseToolkit):
+def register_tools(self, toolkit: Toolkit):
         tools = get_tools_for_toolkit(self.session, toolkit.id)
         existing_tools = [tool.name for tool in tools]
         
