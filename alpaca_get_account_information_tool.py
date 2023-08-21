4494c1282464cd7e742b301920f0b3bf0b315d14
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-08-21 00:11:21 -0700

    Update alpaca_get_account_information_tool.py

--- alpaca_get_account_information_tool.py
+++ alpaca_get_account_information_tool.py
@@ -1,9 +1,39 @@
 
 from typing import Dict
 from superagi.tools.base_tool import BaseTool, tool
-from superagi.types import AlpacaAccount
-from superagi.models import AlpacaConfig
+from pydantic import BaseModel
 from alpaca_trade_api import REST
+
+# Local Model Definitions
+class AlpacaConfig(BaseModel):
+    api_key: str
+    api_secret: str
+    base_url: str
+
+class AlpacaAccount(BaseModel):
+    id: str
+    status: str
+    currency: str
+    buying_power: float
+    cash: float
+    portfolio_value: float
+    pattern_day_trader: bool
+    trading_blocked: bool
+    transfers_blocked: bool
+    account_blocked: bool
+    created_at: str
+    trade_suspended_by_user: bool
+    multiplier: float
+    shorting_enabled: bool
+    equity: float
+    last_equity: float
+    long_market_value: float
+    short_market_value: float
+    initial_margin: float
+    maintenance_margin: float
+    last_maintenance_margin: float
+    sma: float
+    daytrade_count: int
 
 # Define the main tool
 class AlpacaGetAccountInformationTool(BaseTool):
