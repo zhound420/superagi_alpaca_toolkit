9e3efe87a44a23181d82a9bb4bf7aa3e4602f099
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-08-20 21:25:47 -0700

    Update alpaca_toolkit.py

--- alpaca_toolkit.py
+++ alpaca_toolkit.py
@@ -1,10 +1,11 @@
 from superagi.tools.base_tool import BaseToolkit, BaseTool
 from typing import List, Type
 from superagi.tools.external_tools.superagi_alpaca_toolkit.alpaca_close_all_trades_tool import AlpacaCloseAllTradesTool
-from superagi.tools.external_tools.superagi_alpaca_toolkit.alpaca_get_account_tool import AlpacaGetAccountTool
+from superagi.tools.external_tools.superagi_alpaca_toolkit.alpaca_get_account_information_tool import AlpacaGetAccountTool
 from superagi.tools.external_tools.superagi_alpaca_toolkit.alpaca_get_day_percent_change_tool import AlpacaGetDayPercentChangeTool
-from superagi.tools.external_tools.superagi_alpaca_toolkit.alpaca_list_positions_tool import AlpacaListPositionsTool
+from superagi.tools.external_tools.superagi_alpaca_toolkit.alpaca_get_positions_tool import AlpacaListPositionsTool
 from superagi.tools.external_tools.superagi_alpaca_toolkit.alpaca_market_data_tool import AlpacaMarketDataTool
+from .alpaca_get_account_tool import AlpacaGetAccountTool
 
 class AlpacaToolkit(BaseToolkit):
     name = "Alpaca Toolkit"
