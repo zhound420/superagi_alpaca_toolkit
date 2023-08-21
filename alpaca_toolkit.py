955a917050abbc897e52a671167a273423339089
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-08-20 19:36:59 -0700

    Update alpaca_toolkit.py

--- alpaca_toolkit.py
+++ alpaca_toolkit.py
@@ -1,20 +1,23 @@
-
 from superagi.tools.base_tool import BaseToolkit, BaseTool
-from typing import List, Optional
-from .alpaca_close_all_trades_tool import AlpacaCloseAllTradesTool
-from .alpaca_get_account_tool import AlpacaGetAccountTool
-from .alpaca_get_day_percent_change_tool import AlpacaGetDayPercentChangeTool
-from .alpaca_list_positions_tool import AlpacaListPositionsTool
-from .alpaca_market_data_tool import AlpacaMarketDataTool
+from typing import List, Type
+from superagi.tools.external_tools.superagi_alpaca_toolkit.alpaca_close_all_trades_tool import AlpacaCloseAllTradesTool
+from superagi.tools.external_tools.superagi_alpaca_toolkit.alpaca_get_account_tool import AlpacaGetAccountTool
+from superagi.tools.external_tools.superagi_alpaca_toolkit.alpaca_get_day_percent_change_tool import AlpacaGetDayPercentChangeTool
+from superagi.tools.external_tools.superagi_alpaca_toolkit.alpaca_list_positions_tool import AlpacaListPositionsTool
+from superagi.tools.external_tools.superagi_alpaca_toolkit.alpaca_market_data_tool import AlpacaMarketDataTool
 
-
-class AlpacaToolkit(BaseToolkit, ABC):
+class AlpacaToolkit(BaseToolkit):
     name = "Alpaca Toolkit"
     description = "Toolkit for interacting with Alpaca API"
 
     def get_tools(self) -> List[Type[BaseTool]]:
-        return [AlpacaCloseAllTradesTool, AlpacaPlaceTradeTool, AlpacaCheckPriceChangesTool, AlpacaCloseTradeTool, AlpacaGetDayPercentChangeTool, AlpacaMonitorTool, AlpacaGetAccountInformationTool, AlpacaGetPositionsTool, AlpacaGetCurrentPriceTool
-    ]
+        return [
+            AlpacaCloseAllTradesTool, 
+            AlpacaGetAccountTool, 
+            AlpacaGetDayPercentChangeTool, 
+            AlpacaListPositionsTool, 
+            AlpacaMarketDataTool
+        ]
 
     def get_env_keys(self) -> List[str]:
         return []
