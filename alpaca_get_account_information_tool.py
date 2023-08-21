0e853c2f5e1a414495a345f221a0d823a3d7715e
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-08-20 22:54:41 -0700

    Update alpaca_get_account_information_tool.py

--- alpaca_get_account_information_tool.py
+++ alpaca_get_account_information_tool.py
@@ -1,17 +1,23 @@
 
-from superagi.tools.base_tool import BaseTool, tool
-from alpaca import REST
-from superagi.tools.base_tool import ToolConfig
 from typing import Dict
 
+from superagi.models.base_tool import BaseTool, tool
+from alpaca.trading.client import TradingClient
+
+
 class AlpacaGetAccountInformationTool(BaseTool):
-    name = "AlpacaGetAccountInformationTool"
-    description = "Retrieve Alpaca account information"
+    name = "alpaca_get_account_information_tool"
+    description = "Fetches account details for the Alpaca trading platform."
+
+    def __init__(self):
+        super().__init__()
+        self.base_url = self.get_tool_config("ALPACA_BASE_URL")
+        self.api_key = self.get_tool_config("ALPACA_API_KEY")
+        self.secret_key = self.get_tool_config("ALPACA_SECRET_KEY")
 
     @tool()
-    def _execute(self) -> Dict:
-        api = REST(self.get_tool_config(ToolConfig("ALPACA_API_KEY")),
-                   self.get_tool_config(ToolConfig("ALPACA_SECRET_KEY")),
-                   base_url=self.get_tool_config(ToolConfig("ALPACA_BASE_URL")))
-        account_info = api.get_account()
-        return account_info._raw
+    def execute(self, input_data: Dict) -> Dict:
+        trading_client = TradingClient(self.api_key, self.secret_key, base_url=self.base_url)
+        account = trading_client.get_account()
+        return account.to_dict()
+
