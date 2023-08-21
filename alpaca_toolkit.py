f67d62078ba4262566e1fa1467e5ca74b2c4c05d
zhound420 <34777398+zhound420@users.noreply.github.com>
2023-08-21 01:21:48 -0700

    Update alpaca_toolkit.py

--- alpaca_toolkit.py
+++ alpaca_toolkit.py
@@ -2,7 +2,7 @@
 from typing import List
 from superagi.tools.external_tools.superagi_alpaca_toolkit.alpaca_get_account_information_tool import AlpacaGetAccountInformationTool
 from superagi.tools.external_tools.superagi_alpaca_toolkit.alpaca_market_data_tool import AlpacaMarketDataTool
-from .alpaca_submit_order_tool import AlpacaSubmitOrderTool
+from superagi.tools.external_tools.superagi_alpaca_toolkit.alpaca_submit_order_tool import AlpacaSubmitOrderTool
 from superagi.tools.base_toolkit import BaseToolkit
 
 class AlpacaToolkit(BaseToolkit):
